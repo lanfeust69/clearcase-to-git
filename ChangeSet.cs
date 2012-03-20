572a329a2208c78e1768a26a1d321940d8eb1385
Jean-Jacques Lafay <jeanjacques.lafay@gmail.com>
2012-03-20 18:41:36 +0100

    ChangeSet Building

--- ChangeSet.cs
+++ ChangeSet.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace GitImporter
 {
@@ -12,6 +13,14 @@ namespace GitImporter
     /// </summary>
     public class ChangeSet
     {
+        public class Comparer : IComparer<ChangeSet>
+        {
+            public int Compare(ChangeSet x, ChangeSet y)
+            {
+                return x.StartTime.CompareTo(y.StartTime);
+            }
+        }
+
         public string Author { get; private set; }
         public string Branch { get; private set; }
 
@@ -19,7 +28,6 @@ namespace GitImporter
         public DateTime FinishTime { get; set; }
 
         public List<ElementVersion> Versions { get; private set; }
-        public string Comment { get; set; }
 
         public ChangeSet(string author, string branch, DateTime time)
         {
@@ -29,6 +37,40 @@ namespace GitImporter
             FinishTime = time;
 
             Versions = new List<ElementVersion>();
+        }
+
+        public void Add(ElementVersion version)
+        {
+            ElementVersion existing = Versions.Find(v => v.Element == version.Element);
+            if (existing != null)
+            {
+                // we are always on the same branch => we keep the latest version number,
+                // which should always be the new version due to the way we retrieve them
+                if (existing.VersionNumber < version.VersionNumber)
+                {
+                    Versions.Remove(existing);
+                    Versions.Add(version);
+                }
+            }
+            else
+                Versions.Add(version);
+            if (version.Date < StartTime)
+                StartTime = version.Date;
+            if (version.Date > FinishTime)
+                FinishTime = version.Date;
+        }
+
+        public string GetComment()
+        {
+            return string.Join("\r\n",
+                Versions.Select(v => new { Name = v.Element.Name, Comment = v.Comment })
+                    .GroupBy(e => e.Comment)
+                    .Select(g => g.Count() > 3 ? g.Key : string.Join(", ", g.Select(p => p.Name)) + " : " + g.Key));
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}@{1} : {2} changes between {3} and {4}", Author, Branch, Versions.Count, StartTime, FinishTime);
         }
     }
 }
--- ChangeSetBuilder.cs
+++ ChangeSetBuilder.cs
@@ -11,6 +11,9 @@ namespace GitImporter
     {
         public static TraceSource Logger = Program.Logger;
 
+        private const int MAX_DELAY = 15;
+        private static readonly ChangeSet.Comparer _comparer = new ChangeSet.Comparer();
+
         private readonly Dictionary<string, Element> _directoryElements;
         private readonly Dictionary<string, Element> _fileElements;
         private readonly Dictionary<string, Element> _elementsByOid;
@@ -32,6 +35,109 @@ namespace GitImporter
 
         public void Build()
         {
+            var changeSets = CreateChangeSets();
+        }
+
+        private Dictionary<string, Dictionary<string, List<ChangeSet>>> CreateChangeSets()
+        {
+            // the list must always be kept sorted, so that BinarySearch works
+            // if the size of the list gets too big and (mostly) linear insertion time becomes a problem,
+            // we could look at SorteList<> (which is not actually a list, but a dictionary)
+            var changeSets = new Dictionary<string, Dictionary<string, List<ChangeSet>>>();
+            foreach (var element in _elementsByOid.Values)
+                foreach (var branch in element.Branches.Values)
+                {
+                    if (branch.BranchName != "main" && _branchFilters != null && !_branchFilters.Any(e => e.IsMatch(branch.BranchName)))
+                        continue;
+                    Dictionary<string, List<ChangeSet>> branchChangeSets;
+                    if (!changeSets.TryGetValue(branch.BranchName, out branchChangeSets))
+                    {
+                        branchChangeSets = new Dictionary<string, List<ChangeSet>>();
+                        changeSets.Add(branch.BranchName, branchChangeSets);
+                    }
+                    foreach (var version in branch.Versions)
+                    {
+                        if (version.VersionNumber == 0)
+                            continue;
+                        List<ChangeSet> authorChangeSets;
+                        if (!branchChangeSets.TryGetValue(version.Author, out authorChangeSets))
+                        {
+                            authorChangeSets = new List<ChangeSet>();
+                            branchChangeSets.Add(version.Author, authorChangeSets);
+                        }
+                        AddVersion(authorChangeSets, version);
+                    }
+                }
+            return changeSets;
+        }
+
+        private static void AddVersion(List<ChangeSet> changeSets, ElementVersion version)
+        {
+            // used either for search or for new ChangeSet
+            var changeSet = new ChangeSet(version.Author, version.Branch.BranchName, version.Date);
+            if (changeSets.Count == 0)
+            {
+                changeSet.Add(version);
+                changeSets.Add(changeSet);
+                return;
+            }
+
+            int index = changeSets.BinarySearch(changeSet, _comparer);
+            if (index >= 0)
+            {
+                changeSets[index].Add(version);
+                return;
+            }
+
+            index = ~index; // index of first element bigger
+            if (index == changeSets.Count)
+            {
+                // so even the last one is not bigger
+                ChangeSet candidate = changeSets[index - 1];
+                if (version.Date <= candidate.FinishTime.AddSeconds(MAX_DELAY))
+                    candidate.Add(version);
+                else
+                {
+                    changeSet.Add(version);
+                    changeSets.Add(changeSet);
+                }
+                return;
+            }
+            if (index == 0)
+            {
+                ChangeSet candidate = changeSets[0];
+                if (version.Date >= candidate.StartTime.AddSeconds(-MAX_DELAY))
+                    candidate.Add(version);
+                else
+                {
+                    changeSet.Add(version);
+                    changeSets.Insert(0, changeSet);
+                }
+                return;
+            }
+            DateTime lowerBound = changeSets[index - 1].FinishTime;
+            DateTime upperBound = changeSets[index].StartTime;
+            if (version.Date <= lowerBound.AddSeconds(MAX_DELAY) && version.Date < upperBound.AddSeconds(-MAX_DELAY))
+            {
+                changeSets[index - 1].Add(version);
+                return;
+            }
+            if (version.Date > lowerBound.AddSeconds(MAX_DELAY) && version.Date >= upperBound.AddSeconds(-MAX_DELAY))
+            {
+                changeSets[index].Add(version);
+                return;
+            }
+            if (version.Date > lowerBound.AddSeconds(MAX_DELAY) && version.Date < upperBound.AddSeconds(-MAX_DELAY))
+            {
+                changeSet.Add(version);
+                changeSets.Insert(index, changeSet);
+                return;
+            }
+            // last case : we should merge the two ChangeSets (that are now "linked" by the version we are adding)
+            changeSets[index - 1].Add(version);
+            foreach (var v in changeSets[index].Versions)
+                changeSets[index - 1].Add(v);
+            changeSets.RemoveAt(index);
         }
     }
 }
