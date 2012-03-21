18a800255ddeefc8a786b478faea8c4081a10677
Jean-Jacques Lafay <jeanjacques.lafay@gmail.com>
2012-03-21 09:50:05 +0100

    Un from pour les branches

--- GitWriter.cs
+++ GitWriter.cs
@@ -22,6 +22,7 @@ namespace GitImporter
             int n = 0;
             Logger.TraceData(TraceEventType.Start | TraceEventType.Information, (int)TraceId.ApplyChangeSet, "Start writing " + total + " change sets");
 
+            var startedBranches = new HashSet<string>();
             foreach (var changeSet in changeSets)
             {
                 n++;
@@ -30,20 +31,25 @@ namespace GitImporter
                     //Logger.TraceData(TraceEventType.Information, (int)TraceId.ApplyChangeSet, "Writing change set " + n + " of " + total);
 
                 Logger.TraceData(TraceEventType.Start | TraceEventType.Verbose, (int)TraceId.ApplyChangeSet, "Start writing change set", n);
-                WriteChangeSet(changeSet);
+                WriteChangeSet(changeSet, n, !startedBranches.Contains(changeSet.Branch));
+                startedBranches.Add(changeSet.Branch);
                 Logger.TraceData(TraceEventType.Stop | TraceEventType.Verbose, (int)TraceId.ApplyChangeSet, "Stop writing change set", n);
             }
 
             Logger.TraceData(TraceEventType.Stop | TraceEventType.Information, (int)TraceId.ApplyChangeSet, "Stop writing " + total + " change sets");
         }
 
-        private void WriteChangeSet(ChangeSet changeSet)
+        private void WriteChangeSet(ChangeSet changeSet, int commitNumber, bool isNewBranch)
         {
-            _writer.Write("commit refs/heads/" + (changeSet.Branch == "main" ? "master" : changeSet.Branch) + "\n");
+            string branchName = changeSet.Branch == "main" ? "master" : changeSet.Branch;
+            _writer.Write("commit refs/heads/" + branchName + "\n");
+            _writer.Write("mark :" + commitNumber + "\n");
             _writer.Write("committer " + changeSet.Author + " <" + changeSet.Author + "@sgcib.com> " + (changeSet.StartTime - _epoch).TotalSeconds + " +0200\n");
-            // TODO : handle from for non-main (master) branches
             string comment = changeSet.GetComment();
             _writer.Write("data " + comment.Length + "\n" + comment + "\n");
+            if (isNewBranch && branchName != "master" && commitNumber > 1)
+                // TODO : commitNumber - 1 is not always correct (for instance if two branches start from the same master commit)
+                _writer.Write("from :" + (commitNumber - 1) + "\n");
             foreach (var version in changeSet.Versions)
             {
                 // TODO : handle directories
